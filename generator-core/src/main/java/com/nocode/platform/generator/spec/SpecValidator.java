7313573a54b9f9c2843815ab0178ad33e43bb525
artem <evko_artem@mail.ru>
2026-02-28 16:26:11 +0300

    Обновление валидации Spec (generator-core/.../SpecValidator.java)

--- generator-core/src/main/java/com/nocode/platform/generator/spec/SpecValidator.java
+++ generator-core/src/main/java/com/nocode/platform/generator/spec/SpecValidator.java
@@ -17,7 +17,36 @@ public class SpecValidator {
             if (blank(spec.project().basePackage())) errors.add("project.basePackage is required");
         }
 
-        if (spec.entities() == null) errors.add("entities must be present (can be empty list)");
+        if (spec.entities() == null) {
+            errors.add("entities must be present (can be empty list)");
+        } else {
+            List<String> entityNames = spec.entities().stream().map(Spec.Entity::name).toList();
+            for (int i = 0; i < spec.entities().size(); i++) {
+                Spec.Entity entity = spec.entities().get(i);
+                if (blank(entity.name())) errors.add("entities[" + i + "].name is required");
+                
+                if (entity.fields() != null) {
+                    for (int j = 0; j < entity.fields().size(); j++) {
+                        Spec.Field field = entity.fields().get(j);
+                        if (blank(field.name())) errors.add("entities[" + i + "].fields[" + j + "].name is required");
+                        if (field.type() == null) errors.add("entities[" + i + "].fields[" + j + "].type is required");
+                    }
+                }
+                
+                if (entity.relations() != null) {
+                    for (int j = 0; j < entity.relations().size(); j++) {
+                        Spec.Relation rel = entity.relations().get(j);
+                        if (blank(rel.name())) errors.add("entities[" + i + "].relations[" + j + "].name is required");
+                        if (rel.type() == null) errors.add("entities[" + i + "].relations[" + j + "].type is required");
+                        if (blank(rel.targetEntity())) {
+                            errors.add("entities[" + i + "].relations[" + j + "].targetEntity is required");
+                        } else if (!entityNames.contains(rel.targetEntity())) {
+                            errors.add("entities[" + i + "].relations[" + j + "].targetEntity '" + rel.targetEntity() + "' does not exist");
+                        }
+                    }
+                }
+            }
+        }
 
         if (!errors.isEmpty()) {
             throw new IllegalArgumentException("Spec validation failed:\n- " + String.join("\n- ", errors));
