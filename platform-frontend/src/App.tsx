0f3227bfb9310d9e9b1a2cc30e1bffcc60e17abf
artem <evko_artem@mail.ru>
2026-03-01 12:40:07 +0300

    Fix React StrictMode state mutation bug in ModelerPage

--- platform-frontend/src/App.tsx
+++ platform-frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import ProjectsPage from './pages/ProjectsPage';
+import ModelerPage from './pages/ModelerPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/projects" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:projectId/modeler" element={<ModelerPage />} />
+        <Route path="/" element={<Navigate to="/projects" replace />} />
       </Routes>
     </BrowserRouter>
   );
